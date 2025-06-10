f0ff687cd7e84700c9c7bfaf1aabaa2143cc6d86
jfg0029 <jfg0029@alu.medac.es>
2025-06-10 21:13:55 +0200

    Implementado selector de numero para edad y peso

--- lib/screens/imc_home_screen.dart
+++ lib/screens/imc_home_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:imc_calculator/components/gender_selector.dart';
 import 'package:imc_calculator/components/height_selector.dart';
+import 'package:imc_calculator/components/number_selector.dart';
+import 'package:imc_calculator/core/app_colors.dart';
+import 'package:imc_calculator/core/text_styles.dart';
 
 class ImcHomeScreen extends StatefulWidget {
   const ImcHomeScreen({super.key});
@@ -10,12 +13,74 @@ class ImcHomeScreen extends StatefulWidget {
 }
 
 class _ImcHomeScreenState extends State<ImcHomeScreen> {
+  int selectedAge = 20;
+  int selectedWeight = 74;
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         GenderSelector(),
         HeightSelector(),
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Row(
+            children: [
+              Expanded(
+                child: NumberSelector(
+                  title: "PESO",
+                  value: selectedWeight,
+                  onIncrement: () {
+                    setState(() {
+                      selectedWeight++;
+                    });
+                  },
+                  onDecrement: () {
+                    setState(() {
+                      selectedWeight--;
+                    });
+                  },
+                ),
+              ),
+              SizedBox(width: 16),
+              Expanded(
+                child: NumberSelector(
+                  title: "EDAD",
+                  value: selectedAge,
+                  onIncrement: () {
+                    setState(() {
+                      selectedAge++;
+                    });
+                  },
+                  onDecrement: () {
+                    setState(() {
+                      selectedAge--;
+                    });
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+        Spacer(),
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: SizedBox(
+            height: 60,
+            width: double.infinity,
+
+            child: ElevatedButton(
+              onPressed: () {},
+              style: ButtonStyle(
+                shape: WidgetStateProperty.all(RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10)
+                )),
+                backgroundColor: WidgetStateProperty.all(AppColors.primary),
+              ),
+              child: Text("Calcular IMC", style: TextStyles.bodyText),
+            ),
+          ),
+        ),
       ],
     );
   }
